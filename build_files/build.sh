17db259872ae326a6a999e3b146d08b2e48165ba
Yusef Aslam <YUZi54780@outlook.com>
2026-04-25 18:32:29 +0100

    feat(build.sh): add programs to install

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux emacs ripgrep fd-find
 
 # Use a COPR Example:
 #
@@ -18,6 +18,10 @@ dnf5 install -y tmux
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+dnf5 copr enable avengemedia/dms
+dnf5 install dms
+
+dnf5 -y copr disable avengemedia/dms
 
 #### Example for enabling a System Unit File
 
